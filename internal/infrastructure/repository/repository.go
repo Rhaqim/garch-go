236629ab0983c228e2f7f0ed65df3abbbd89cecd
Rhaqim <anusiemj@gmail.com>
2024-02-24 20:21:30 +0100

    adding architectures and gitignore

--- internal/infrastructure/repository/repository.go
+++ internal/infrastructure/repository/repository.go
@@ -1,1 +1,8 @@
 package repository
+
+import (
+	"github.com/Rhaqim/garch-go/internal/app/core"
+	architecture "github.com/Rhaqim/garch-go/internal/infrastructure/architectures"
+)
+
+var HexRootFiles []core.FileStructure = architecture.RootFiles
